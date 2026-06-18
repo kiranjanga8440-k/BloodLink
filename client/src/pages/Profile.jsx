612d68d845fe2e78f8abfc5da0f1937d73590672
kiranjanga8440-k <kiranjanga8440@gmail.com>
2026-06-18 14:36:13 +0530

    Added admin emergency requests panel

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -1,32 +1,7 @@
-function Profile({ donor }) {
+function Profile() {
   return (
-    <div className="max-w-md mx-auto mt-8 bg-white p-6 rounded-xl shadow">
-
-      <h2 className="text-2xl font-bold mb-4">
-        Donor Profile
-      </h2>
-
-      <p><b>Name:</b> {donor.name}</p>
-      <p><b>Age:</b> {donor.age}</p>
-      <p><b>Blood Group:</b> {donor.bloodGroup}</p>
-
-      <p>
-        <b>Status:</b>
-        {donor.verified ? " ✅ Verified" : " ❌ Not Verified"}
-      </p>
-
-      <p>
-        <b>Last Donation:</b>
-        {donor.lastDonationDate
-          ? new Date(donor.lastDonationDate).toLocaleDateString()
-          : "Never Donated"}
-      </p>
-
-      <p>
-        <b>Total Donations:</b>
-        {donor.totalDonations}
-      </p>
-
+    <div>
+      <h1>Profile Page Working ✅</h1>
     </div>
   );
 }
