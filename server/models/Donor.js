a69e9b323ad6fe926a86c2e8d73968bd5cc2cc4d
kiranjanga8440-k <kiranjanga8440@gmail.com>
2026-06-20 10:41:37 +0530

    Added admin emergency requests panel

--- server/models/Donor.js
+++ server/models/Donor.js
@@ -5,46 +5,60 @@ const donorSchema = new mongoose.Schema({
     type: String,
     required: true,
   },
+
   email: {
     type: String,
     required: true,
     unique: true,
   },
+
   age: {
-  type: Number,
-  required: true
+    type: Number,
+    required: true,
   },
+
   bloodGroup: {
     type: String,
     required: true,
   },
+
   phone: {
     type: String,
     required: true,
     unique: true,
   },
+
   city: {
     type: String,
     required: true,
   },
+
   aadhaarDocument: {
     type: String,
     default: "",
   },
+
   verified: {
     type: Boolean,
-    default: true
+    default: true,
+  },
+
+  available: {
+    type: Boolean,
+    default: true,
   },
 
   lastDonationDate: {
     type: Date,
-    default: null
+    default: null,
   },
 
   totalDonations: {
     type: Number,
-    default: 0
-  }
+    default: 0,
+  },
+}, {
+  timestamps: true,
 });
 
 module.exports = mongoose.model("Donor", donorSchema);
